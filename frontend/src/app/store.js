7a619a924a673845b00757e85f24d27700a26742
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-16 01:30:30 +0600

    login and signup rtk query

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { loginApi } from "../services/loginApi";
+import { signupApi } from "../services/signupApi";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [loginApi.reducerPath]: loginApi.reducer,
+    [signupApi.reducerPath]: signupApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loginApi.middleware, signupApi.middleware)
 });
+
+setupListeners(store.dispatch);
